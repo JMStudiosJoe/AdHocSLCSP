01504f794a59b8fabf21a5437461360fd8316b00
Joseph <jrichardson@jmstudios.net>
2017-09-10 10:49:56 -0700

    getting list of dict objects from csv

--- csv_reader.py
+++ csv_reader.py
@@ -2,13 +2,21 @@ import csv
 
 class CSVReader():
 
-    def __init__(self, path_to_csv):
+    def read_csv(self, path_to_csv):
         with open(path_to_csv) as csv_file:
-            print(csv_file)
+            dict_reader = csv.DictReader(csv_file)
+            return list(dict_reader)
 
+# slcsp may not be found if silver plan not available in the area or if only one
+# cost in the area?
 
 if __name__ == '__main__':
-    slcsp_csv = CSVReader('./slcsp.csv')
-    plans_csv = CSVReader('./plans.csv')
-    zips_csv = CSVReader('./zips.csv')
+    # process
+    # 1. load all the zips
+    # 2. go through plans and find plans for all zips
+    reader = CSVReader()
+    zips_csv = reader.read_csv('./zips.csv')
+    import pdb;pdb.set_trace()
+    plans_csv = reader.read_csv('./plans.csv')
+    slcsp_csv = reader.read_csv('./slcsp.csv')
 
